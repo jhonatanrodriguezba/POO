c502f1a06569eedc2a6984b2eb6c7a2c3eb5bac1
jhonatanrodriguezba <jhonatan.rodriguezba@campusucc.edu.co>
2025-11-04 14:57:39 +0000

    Clase Piso Hecho

--- Momento3/Piso.java
+++ Momento3/Piso.java
@@ -4,4 +4,19 @@ public class Piso {
     private int num_piso;
     private Boton b_subida;
     private Boton b_bajada;
+
+    public Piso (int num_piso){
+        this.num_piso = num_piso;
+        b_subida = new Boton("Subida");
+        b_bajada = new Boton("Bajada");
+    }
+    public int getNumero(){
+        return num_piso;
+    }
+    public Boton getb_subida(){
+        return b_subida;
+    }
+    public Boton getb_bajada(){
+        return b_bajada;
+    }
 }
