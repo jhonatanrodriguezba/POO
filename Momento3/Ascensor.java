968888f1d379ef83cca19c7e380ef44db889ad36
jhonatanrodriguezba <jhonatan.rodriguezba@campusucc.edu.co>
2025-11-11 21:07:01 +0000

    rediseño Clase ascensor

--- Momento3/Ascensor.java
+++ Momento3/Ascensor.java
@@ -1,51 +1,61 @@
-import java.util.ArrayList;
-
 public class Ascensor {
     private int pisoActual;
     private String direccion;
     private boolean enMovimiento;
     private Puerta puerta;
-    private ArrayList<Boton> botonesInteriores;
+    private Boton botonEmergencia;
 
-    public Ascensor (int cantidadDePisos){
+    public Ascensor() {
         this.pisoActual = 0;
-        this.enMovimiento = false;
         this.direccion = "detenido";
+        this.enMovimiento = false;
         this.puerta = new Puerta();
-        this.botonesInteriores = new ArrayList<>();
+        this.botonEmergencia = new Boton("emergencia");
+    }
 
-        for (int i = 0; i < cantidadDePisos; i++){
-            botonesInteriores.add(new Boton(direccion));
-        }
-    }
-    public void seleccioPiso(int pisoDestino){
-        System.out.println("Piso " + pisoDestino + "Seleccionado");
-        mover(pisoDestino);
-    }
-    public void mover(int pisoDestino){
-        if (pisoDestino == pisoActual){
-            System.out.println("Estas en este piso");
+    public void moverA(int pisoDestino) {
+        if (pisoDestino == pisoActual) {
+            System.out.println("El ascensor ya está en el piso " + pisoActual);
             return;
         }
-    
+
         enMovimiento = true;
-        direccion = pisoDestino > pisoActual ? "Subiendo" : "Bajando";
-        System.out.println("Ascensor " + direccion + "Hacia piso " + pisoDestino);
+        direccion = pisoDestino > pisoActual ? "subiendo" : "bajando";
+        System.out.println("Ascensor " + direccion + " hacia el piso " + pisoDestino + "...");
+        if (pisoDestino == 0){
+            System.out.println("Ascensot hacia el sotano (0) ");
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         pisoActual = pisoDestino;
         enMovimiento = false;
         direccion = "detenido";
-        System.err.println("El Ascensor ha llegado a: " + pisoActual);
+        System.out.println("El Ascensor ha llegado al piso " + pisoActual);
         puerta.abrir();
-        }
-        public void abrirPuerta(){
-            puerta.abrir();
-        }
-        public void cerrarPuerta(){
-            puerta.cerrar();
-        }
-        public int getPisoActual(){
-            return pisoActual;
-        }
     }
 
+    public void activarEmergencia() {
+        botonEmergencia.presionar();
+        System.out.println("El Ascensor se detuvo, debido a que presionaste el boton de emergencia.");
+        puerta.abrir();
+        enMovimiento = false;
+        direccion = "detenido";
+    }
+
+    public void cerrarPuerta() {
+        puerta.cerrar();
+    }
+
+    public int getPisoActual() {
+        return pisoActual;
+    }
+
+    public boolean enEmergencia() {
+        return botonEmergencia.estaPresionado();
+    }
+}
