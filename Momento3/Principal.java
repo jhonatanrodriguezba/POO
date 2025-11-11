edbeccaf64e8370e4d5679642be9333df419c5e5
jhonatanrodriguezba <jhonatan.rodriguezba@campusucc.edu.co>
2025-11-11 21:19:35 +0000

    Rediseño de la clase de ejecucion (principal)

--- Momento3/Principal.java
+++ Momento3/Principal.java
@@ -1,11 +1,7 @@
 public class Principal {
     public static void main(String[] args) {
-        ControlAscensor sistema = new ControlAscensor(5);
-
-        sistema.mostrarEstado();
-        sistema.solicitarAscensor(3, "subida");
-        sistema.seleccionarPisoDesdeAscensor(1);
-        sistema.mostrarEstado();
+        SistemaControlDelAscensor sist = new SistemaControlDelAscensor(5);
+        sist.iniciar();
     }
 }
 
